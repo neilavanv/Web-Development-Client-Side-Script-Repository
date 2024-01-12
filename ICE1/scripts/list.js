b3802d86829375c6c029524523f674a5a13bbd8d
neilavanv <146143252+neilavanv@users.noreply.github.com>
2024-01-11 20:57:29 -0500

    list.js updated
    This contains all the modifications that had to be made on the list.js file.

--- ICE1/scripts/list.js
+++ ICE1/scripts/list.js
@@ -4,33 +4,44 @@ var groceryList = document.getElementById("groceries");
 
 // ADD NEW ITEM TO END OF LIST
 // Create element
-const newListItem = document.createElement("li");
+const newListItemEnd = document.createElement("li");
 // Create text node
-const listItemContent = document.newTextNode("");
+const listItemContent = document.createTextNode("Kombucha");
 // Add text node to element
-newListItem.appendChild(listItemContent);
+newListItemEnd.appendChild(listItemContent);
 // Add element end of list
-groceryList.appendChild(newListItem);
+groceryList.appendChild(newListItemEnd);
 
 // ADD NEW ITEM START OF LIST
 // Create element
+const newListItemStart = document.createElement("li");
 // Create text node
+const listItemContentStart = document.createTextNode("White Mangoes")
 // Add text node to element
+newListItemStart.appendChild(listItemContentStart)
 // Add element to list
-
+groceryList.insertBefore(newListItemStart, groceryList.firstChild);
 
 
 // All <li> elements
-
+var allListedItems = groceryList.getElementsByTagName("li");
 // ADD A CLASS OF COOL TO ALL LIST ITEMS
 // Counter variable
 // Loop through elements
+    for(var i = 0; i < allListedItems; i++)
+        {
+            allListedItems[i].classList.add("cool");
+        }
 // Change class to cool
 
 
 // ADD NUMBER OF ITEMS IN THE LIST TO THE HEADING
 // h2 element
+var heading = document.querySelector("h2");
 // h2 text
 // No. of <li> elements
+var itemCounter = allListedItems.length;
 // Content
+var contentHeading = "Buy Groceries (" + itemCounter + " items)";
 // Update h2 using innerHTML (not textContent) because it contains markup
+heading.innerHTML = contentHeading;
